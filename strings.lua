0472394b75dbf1a7e1a35027eaa41d93ceec984c
Florian Thiel <flth@flth.de>
2023-07-09 14:29:24 +0200

    initial

---
+++ strings.lua
@@ -0,0 +1,44 @@
+function data()
+return {
+	en = {
+		["mod_name"] = "Rlmms 56 / Kbs 442 / Kbgs 442 Stake cars",
+		["mod_desc"] = "From 1958, the Deutsche Bundesbahn procured two-axle flat wagons according to the UIC standard. The wagons with stakes are designated Rlmms 56, those without stakes Rlmmso 56. The wagons are mainly used to transport vehicles and goods with large space requirements. All wagons will be taken out of service by 2020.",
+
+        ["Kbs442_menu"] = "Rlmms 56 / Kbs 442 / Kbgs 442",
+		["Kbgs442_mit_name"] = "Kbgs 442 with stakes DB (1978)",
+        ["Kbgs442_name"] = "Kbgs 442 DB (1978)",
+		["Kbgs442_desc"] = "In order to have enough wagons available at short notice for container transport, 500 Kbs 442 are fitted with container locks and re-designated as Kbgs 442. These remained in service until 1994.",
+        ["Rlmms56_name"] = "Rlmms 56 DB (1958)",
+        ["Rlmms56_desc"] = "The two-axle wagons with stakes are listed as Rlmms 56 until 1965. They are used to transport vehicles and goods with large space requirements.",
+        ["Kbs442_name"] = "Kbs 442 DB (1965)",
+        ["Kbs442_desc"] = "According to UIC standard, the Rlmms 56 were re-designated as Kbs 442 from 1965.",
+        ["Kbs442_dbag_name"] = "Kbs 442 DB AG (1994)",
+        ["Kbs442_Kls442_dbag_desc"] = "Deutsche Bahn took over the Kbs 442 and Kls 442 wagons and continued to use them until 2020.",
+        ["Rlmmso56_name"] = "Rlmmso 56 DB (1958)",
+        ["Rlmmso56_desc"] = "The two-axle wagons without stakes are listed as Rlmmso 56 until 1965. They are used to transport vehicles and goods with large space requirements.",
+        ["Kls442_name"] = "Kls 442 DB (1965)",
+        ["Kls442_desc"] = "According to UIC standard, the Rlmmso 56 were re-designated as Kls 442 from 1965.",
+        ["Kls442_dbag_name"] = "Kls 442 DB AG (1994)",
+	},
+	de = {
+		["mod_name"] = "Rlmms 56 / Kbs 442 / Kbgs 442 Rungenwagen",
+		["mod_desc"] = "Ab 1958 beschaffte die Deutsche Bundesbahn zweiachsige Flachwagen nach UIC-Standard. Die Wagen mit Rungen werden als Rlmms 56 bezeichnet, die ohne Rungen als Rlmmso 56. Die Wagen dienen vor allem zur Beförderung von Fahrzeugen und Gütern mit großem Raumbedarf. Bis 2020 werden alle Wagen ausgemustert.",
+
+        ["Kbs442_menu"] = "Rlmms 56 / Kbs 442 / Kbgs 442",
+		["Kbgs442_mit_name"] = "Kbgs 442 mit Rungen DB (1978)",
+        ["Kbgs442_name"] = "Kbgs 442 DB (1978)",
+		["Kbgs442_desc"] = "Damit kurzfristig genügend Wagen für den Containertransport zur Verfügung stehen, werden 500 Kbs 442 mit Container-Arretierungen versehen und in Kbgs 442 umgezeichnet. Diese blieben bis 1994 im Einsatz.",
+        ["Rlmms56_name"] = "Rlmms 56 DB (1958)",
+        ["Rlmms56_desc"] = "Die zweiachsigen Wagen mit Rungen werden als Rlmms 56 geführt bis 1965. Sie dienen zum Transport von Fahrzeugen und Gütern mit großem Raumbedarf.",
+        ["Kbs442_name"] = "Kbs 442 DB (1965)",
+        ["Kbs442_desc"] = "Nach UIC-Standard wurden die Rlmms 56 ab 1965 als Kbs 442 umgezeichnet.",
+        ["Kbs442_dbag_name"] = "Kbs 442 DB AG (1994)",
+        ["Kbs442_Kls442_dbag_desc"] = "Die Deutsche Bahn AG übernahm die Kbs 442 und Kls 442-Wagen und setzte sie noch bis zum Jahr 2020 ein.",
+        ["Rlmmso56_name"] = "Rlmmso 56 DB (1958)",
+        ["Rlmmso56_desc"] = "Die zweiachsigen Wagen ohne Rungen werden als Rlmmso 56 geführt bis 1965. Sie dienen zum Transport von Fahrzeugen und Gütern mit großem Raumbedarf.",
+        ["Kls442_name"] = "Kls 442 DB (1965)",
+        ["Kls442_desc"] = "Nach UIC-Standard wurden die Rlmmso 56 ab 1965 als Kls 442 umgezeichnet.",
+        ["Kls442_dbag_name"] = "Kls 442 DB AG (1994)",
+	},
+}
+end
